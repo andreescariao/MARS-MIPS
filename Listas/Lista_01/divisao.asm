df55365ff2dcc6853e44314c758abd6357549bc9
André Escarião <andre.escariao1@gmail.com>
2022-08-06 19:06:58 -0300

    Update divisao.asm

--- Listas/Lista_01/divisao.asm
+++ Listas/Lista_01/divisao.asm
@@ -1,19 +1,18 @@
-# DIVIS√O DE INTEIROS
-# div $t0, $t1 -- Realiza a divis„o de $t0 por $t1 -- a parte inteira vai para lo -- o resto vai para hi
+# DIVIS√ÉO DE INTEIROS
+# div $t0, $t1 -- Realiza a divis√£o de $t0 por $t1 -- a parte inteira vai para lo -- o resto vai para hi
 # mflo $t0 -- move o conteudo de lo para $1
 # mfhi $t1 -- move o conteudo de hi para $t1
 
-.text # ¡rea para instruÁıes do programa
+.text # √Årea para instru√ß√µes do programa
 	li $t0, 32
+	li $t1, 5
 	
-	# Move duas casas para direita -- ou divide por 4
-	srl $s2, $t0, 2
-	
+	div $t0, $t1
 	# Parte inteira em $s0
 	mflo $s0
 	# Resto em $s1
 	mfhi $s1
-	
 	syscall
+	
 	li $v0, 10 # Encerrar o programa
 	syscall
